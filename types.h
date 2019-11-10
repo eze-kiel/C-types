31677a179dadf2593836a945c1189dd10a16c257
Hugo <hugblanc69@gmail.com>
2019-11-10 13:51:23 +0100

    first commit

---
+++ types.h
@@ -0,0 +1,26 @@
+/*
+    NAME:   types.h
+    AUTHOR: ezekiel
+*/
+
+// single byte types
+#define char_t char
+#define uchar_t unsigned char
+
+// two bytes types
+#define int2_t short
+#define uint2_t unsigned short
+
+// 4 bytes types
+#define int4_t int
+#define uint4_t unsigned int
+
+// 8 bytes types
+#define int8_t long long
+#define unint8_t unsigned long long
+ 
+ // float single precision floating point
+ #define float_t float
+
+ // float double precision floating point
+ #define double_t double
